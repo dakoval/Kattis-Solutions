7fd7f2d1584ba4ba854a6ae0c4ff90bd1c1c9547
David Koval <kovaldavid@hotmail.com>
2017-02-25 06:05:22 -0600

    pivot
    remake

--- pivot.java
+++ pivot.java
@@ -17,17 +17,17 @@ public class pivot {
         int ans = 0;
         for (int i = 0, max = Integer.MIN_VALUE; i < n; i++) {
             a[i] = Integer.parseInt(c[i]);
-            b[i] = max;
             if (max < a[i]) {
                 max = a[i];
             }
+            b[i] = max;
         }
         for (int i = n - 1, min = Integer.MAX_VALUE; i >= 0; i--) {
-            if (a[i] > b[i] && a[i] < min) {
-                ans++;
-            }
             if (a[i] < min) {
                 min = a[i];
+            }
+            if (min == b[i]) {
+                ans++;
             }
         }
         System.out.println(ans);
