d6c03aefae23c6e234139e1f3fcaac565d216b8d
David Koval <kovaldavid@hotmail.com>
2017-02-25 05:58:32 -0600

    pivot
    made faster

--- pivot.java
+++ pivot.java
@@ -1,4 +1,3 @@
-
 package Solutions;
 
 /**
@@ -6,25 +5,31 @@ package Solutions;
  * @author David
  */
 import java.io.*;
-import java.util.*;
+
 public class pivot {
-    public static void main(String[] args)throws Exception{
-        Scanner sc=new Scanner(new File("pivot"));
-        while(sc.hasNextInt()){
-            int n =sc.nextInt();
-            int a[]=new int[n];
-            for(int i=0;i<n;i++)a[i]=sc.nextInt();
-            int before[]=new int[n];
-            int output=0;
-            for(int i=0,max=Integer.MIN_VALUE;i<n;i++){
-                before[i]=max;
-                if(max<a[i])max=a[i];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] c = br.readLine().split(" ");
+        int a[] = new int[n];
+        int b[] = new int[n];
+        int ans = 0;
+        for (int i = 0, max = Integer.MIN_VALUE; i < n; i++) {
+            a[i] = Integer.parseInt(c[i]);
+            b[i] = max;
+            if (max < a[i]) {
+                max = a[i];
             }
-            for(int i=n-1,min=Integer.MAX_VALUE;i>=0;i--){
-                if(a[i]>before[i]&&a[i]<min)output++;
-                if(a[i]<min)min=a[i];
-            }
-            System.out.println(output);
         }
+        for (int i = n - 1, min = Integer.MAX_VALUE; i >= 0; i--) {
+            if (a[i] > b[i] && a[i] < min) {
+                ans++;
+            }
+            if (a[i] < min) {
+                min = a[i];
+            }
+        }
+        System.out.println(ans);
     }
 }
