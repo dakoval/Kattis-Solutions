ba544ea7e50e3294cb022c26488499e3cd368897
David Koval <kovaldavid@hotmail.com>
2017-06-14 02:56:10 -0500

    3dprinter

---
+++ _3dprinter.java
@@ -0,0 +1,15 @@
+package Solutions;
+
+/**
+ *
+ * @author Max
+ */
+import java.util.*;
+
+public class _3dprinter {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println((int) (1.999999 + (Math.log(sc.nextInt()) / Math.log(2))));
+    }
+}
