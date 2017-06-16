d1f4ef1a130f2966968ce8f352a2ceff0d205655
David Koval <kovaldavid@hotmail.com>
2017-06-16 04:23:16 -0500

    areal

---
+++ areal.java
@@ -0,0 +1,16 @@
+package Solutions;
+
+/**
+ *
+ * @author Max
+ */
+import java.util.*;
+
+public class areal {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Long n = sc.nextLong();
+        System.out.println((double) Math.sqrt(n) * 4);
+    }
+}
