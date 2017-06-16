e1490183adcd10b3f92945ddc17f42b3a5cb31b6
David Koval <kovaldavid@hotmail.com>
2017-06-16 03:39:15 -0500

    r2

---
+++ r2.java
@@ -0,0 +1,16 @@
+package Solutions;
+
+/**
+ *
+ * @author Max
+ */
+import java.util.*;
+
+public class r2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        System.out.println(b * 2 - a);
+    }
+}
