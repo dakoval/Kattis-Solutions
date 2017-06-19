7c6d8e8579e71070a5309a698b34114a274baa3b
David Koval <kovaldavid@hotmail.com>
2017-06-19 06:10:11 -0500

    herman

---
+++ herman.java
@@ -0,0 +1,16 @@
+package Solutions;
+
+/**
+ *
+ * @author Max
+ */
+import java.util.*;
+
+public class herman {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n * n * Math.PI + "\n" + n * n * 2);
+    }
+}
