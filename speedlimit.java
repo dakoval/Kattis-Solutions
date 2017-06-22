81b73388ca1ad0c68063c2382783c1095a9b0c23
David Koval <kovaldavid@hotmail.com>
2017-06-22 00:02:03 -0500

    speedlimit

--- speedlimit.java
+++ speedlimit.java
@@ -5,25 +5,25 @@
  */
 package Solutions;
 
-/**
- *
- * @author kovalda
- */
 import java.util.*;
-import java.io.*;
+
 public class speedlimit {
-    public static void main(String[] args)throws Exception{
-        Scanner sc = new Scanner(new File("speedlimit.in"));
-        while(true){
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
             int speeds = sc.nextInt();
-            if(speeds==-1)break;
-            int total=0, intime=0, curspeed=0, curtime =0;
-            for(int i=0;i<speeds;i++){
-                curspeed = sc.nextInt(); curtime= sc.nextInt();
-                total += ((curspeed)*(curtime-intime));
+            if (speeds == -1) {
+                break;
+            }
+            int total = 0, intime = 0, curspeed = 0, curtime = 0;
+            for (int i = 0; i < speeds; i++) {
+                curspeed = sc.nextInt();
+                curtime = sc.nextInt();
+                total += ((curspeed) * (curtime - intime));
                 intime = curtime;
             }
-            System.out.println(total+" miles");
+            System.out.println(total + " miles");
         }
     }
 }
